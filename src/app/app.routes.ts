23ef7be5a8b6729ff16abb582d334f4ac8c84a91
Mohamed Abozeid <mohammedabozeid614@gmail.com>
2025-05-28 21:20:40 +0300

    upload the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
+  {
+    path: '/auth',
+    loadComponent: () =>
+      import('./core/layouts/auth/auth.component').then((m) => m.AuthComponent),
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      {
+        path: 'forget-password',
+        loadComponent: () =>
+          import('./core/components/ui/forget/forget.component').then(
+            (m) => m.ForgetComponent
+          ),
+      },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./core/components/ui/login/login.component').then(
+            (m) => m.LoginComponent
+          ),
+      },
+    ],
+  },
+];
